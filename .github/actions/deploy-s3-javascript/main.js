41ea04363f3492b889fa0e7c4a424228ce88c925
praveen <praveendhakade@gmail.com>
2024-08-22 16:03:10 +0530

    js actions s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,17 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 const exec = require("@actions/exec");
 function run() {
+    // get inputs
+    const bucket = core.getInput('bucket', { required: true})
+    const buckerRegion = core.getInput('bucket-region', { required: true})
+    const distFolder = core.getInput('dist-folder', { required: true})
+
+    // github.getOctokit
+    // upload files
+    const s3Uri = `s3://${bucket}` 
+    exec.exec(`aws S3 sync  ${distFolder} ${s3Uri} --region ${buckerRegion}`)
+
     core.notice("Hello custom js actions!")
 }
 
